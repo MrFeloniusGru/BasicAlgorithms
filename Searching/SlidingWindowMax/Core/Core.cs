94e3e1b51e228c004779762ef57981206a5b49e4
MrFeloniusGru <sazstepa@gmail.com>
2023-12-14 00:02:06 +0500

    SlidingWindow

---
+++ Searching/SlidingWindowMax/Core/Core.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SlidingWindowMax;
+
+public class Core<T>
+{
+    private readonly IComparer<T> _comparer;
+    public Core(IComparer<T> comparer)
+    {
+        _comparer = comparer ?? throw new ArgumentNullException(nameof(comparer));
+    }
+
+    public T?[]? Max(T[] numbers, int windowLenght)
+    {
+        if(numbers == null)
+        {
+            return null;
+        }
+
+        if(windowLenght == 1){
+            return numbers.ToArray();
+        }
+
+        if(windowLenght >= numbers.Length)
+        {
+            return new[] { numbers.Max(_comparer) };
+        }
+
+        var window = new LinkedList<int>();
+
+
+        var res = new T[numbers.Length - windowLenght + 1];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            while(window.Any() && window.First != null && window.First.Value < i - windowLenght + 1)
+            {
+                window.RemoveFirst();
+            }
+            while(window.Any() && window.Last != null && _comparer.Compare(numbers[i], numbers[window.Last.Value]) > 0)
+            {
+                window.RemoveLast();
+            }
+
+            window.AddLast(i);
+            if(i >= windowLenght - 1)
+            {
+                res[i - windowLenght + 1] = numbers[window.First!.Value];
+            }
+        }
+
+        return res;
+    }
+}
---
+++ Searching/SlidingWindowMax/Test/SlidingWindowMaxTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests;
+using Xunit;
+using SlidingWindowMax;
+
+public class SlidingWindowMaxTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 4, 3, 2, 1 })]
+
+    public void SortArrayShouldBeCorrect(int[] data)
+    {
+        var core = new Core<int>(Comparer<int>.Default);
+        Assert.Equal(new [] {4, 4, 3}, core.Max(data, 3));
+    }
+
+    [Theory]
+    [InlineData(new int[] { 5, 1 })]
+
+    public void SortArrayShouldBeCorrect2(int[] data)
+    {
+        var core = new Core<int>(Comparer<int>.Default);
+        Assert.Equal(new [] {5}, core.Max(data, 3));
+    }
+
+    [Theory]
+    [InlineData(new int[] { 4, 4, 4, 4 })]
+
+    public void SortArrayShouldBeCorrect3(int[] data)
+    {
+        var core = new Core<int>(Comparer<int>.Default);
+        Assert.Equal(new [] {4, 4}, core.Max(data, 3));
+    }
+}
