f4f7ca4107cf9c5af2e5985c8c7cbd6e3623f427
MrFeloniusGru <sazstepa@gmail.com>
2023-12-14 22:56:13 +0500

    XorMissNumber

---
+++ Searching/XorMissNumber/Tests/XorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+using Xunit;
+using XorMissNumberCore;
+
+public class XorTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 5, 7})]
+
+
+    public void MissTwoNumbersShouldBeCorrect(int[] data)
+    {
+        var core = new Core();
+        Assert.Equal([4,6], core.MissTwoNumbers(data));
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3})]
+    public void MissTwoNumbersShouldBeCorrect2(int[] data)
+    {
+        var core = new Core();
+        Assert.Equal([4,5], core.MissTwoNumbers(data));
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 4})]
+    public void MissOneNumbersShouldBeCorrect(int[] data)
+    {
+        var core = new Core();
+        Assert.Equal(3, core.MissOneNumber(data));
+    }
+}
---
+++ Searching/XorMissNumber/XorMissNumberCore/Core.cs
@@ -0,0 +1,49 @@
+﻿namespace XorMissNumberCore;
+
+public class Core
+{
+
+    private int AriphmeticProgressionSum(int a1, int a2)
+    {
+        return (a1 + a2) * (a2 - a1 + 1) / 2;
+    }
+
+    // O(1)
+    public int? MissOneNumber(int [] numbers)
+    {
+        if(numbers == null)
+        {
+            return null;
+        }
+
+        return AriphmeticProgressionSum(1, numbers.Length + 1) - numbers.Sum();
+    }
+
+    // O(n)
+    public int[]? MissTwoNumbers(int[] numbers)
+    { 
+        if(numbers == null)
+        {
+            return [];
+        }
+
+        // MissOne + MissTwo
+        var sumTwoNumbers = AriphmeticProgressionSum(1, numbers.Length + 2) - numbers.Sum();
+        
+        // MissOne ^ MissTwo
+        var xorTwoNumbers = numbers.Aggregate((x, y) => x ^ y) ^ Enumerable.Range(1, numbers.Length + 2).Aggregate((x, y) => x ^ y);
+
+        for (int i = 1; i < numbers.Length + 3; i++)
+        {
+            // j = MissOne ^ MissOne ^ MissTwo  =>  0 ^ MissTwo  =>  MissTwo
+            var j = i ^ xorTwoNumbers;
+            // MissTwo == MissOne + MissTwo - i => i = MissOne
+            if (j == sumTwoNumbers - i)
+            {
+                return [i, j];
+            }
+        }
+
+        return [];
+    }
+}
