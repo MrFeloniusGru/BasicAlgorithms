40d1b2a001a7177f56de7b68ce886bdfccc237d9
MrFeloniusGru <sazstepa@gmail.com>
2023-12-20 01:07:45 +0500

    EuclidGcd

---
+++ EuclidGcd/EuclidGcdLib/Core.cs
@@ -0,0 +1,27 @@
+﻿namespace EuclidGcdLib;
+
+public class Core
+{
+    // O(lg(b))
+    public int Gcd(int a, int b)
+    {
+        while(b > 0)
+        {
+            (b, a) = (a % b, b);
+        }
+
+        return a;
+    }
+
+    public (int, int, int) GcdExtended(int a, int b)
+    {
+        if(b == 0)
+        {
+            return (a, 1, 0);
+        }
+        
+        int d, x, y;
+        (d, x, y) = GcdExtended(b, a % b);
+        return (d, y, x - a / b * y);
+    }
+}
---
+++ EuclidGcd/Tests/EuclidTests.cs
@@ -0,0 +1,28 @@
+﻿using EuclidGcdLib;
+using Xunit;
+
+namespace Tests;
+
+public class EuclidTests
+{
+    
+    [Theory]
+    [InlineData( 30, 21, 3 )]
+    [InlineData( 10, 0, 10 )]
+    [InlineData( 1, 1, 1 )]
+    [InlineData( 21, 30, 3 )]
+    [InlineData( 144, 12, 12 )]
+    public void Gcd(int a, int b, int answer)
+    {
+        var core = new Core();
+        Assert.Equal(answer, core.Gcd(a, b));
+    }
+
+    [Theory]
+    [InlineData( 99, 78, 3, -11, 14 )]
+    public void GcdExtended(int a, int b, int d, int x, int y)
+    {
+        var core = new Core();
+        Assert.Equal((d, x, y), core.GcdExtended(a, b));
+    }
+}
